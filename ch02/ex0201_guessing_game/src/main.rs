fa023ea1594d61083aa92ddc30648643f4297e84
mindeleven <mindeleven@posteo.de>
2024-03-04 11:03:06 +0100

    Polishing the guessing game a bit

--- ch02/ex0201_guessing_game/src/main.rs
+++ ch02/ex0201_guessing_game/src/main.rs
@@ -1,37 +1,40 @@
-/// Coding along with The Book 
+use rand::Rng;
+use std::cmp::Ordering;
+/// Coding along with The Book
 /// The Rust Programming Language
 /// Chapter 2, Programming a Guessing Game
 /// Source code and comments are based on The Book
-
 use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    println!("The secret number is {}", secret_number);
-    
+
+    // println!("The secret number is {}", secret_number);
+
     println!("It's a guessing game!");
+    loop {
+        println!("Please enter a number:");
 
-    println!("Please enter a number:");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Something went wrong");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Something went wrong");
-        
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Please type a number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("Your input is: {}", guess);
-    
-    match guess.cmp(&secret_number) {
-      Ordering::Less => println!("Too small"),
-      Ordering::Equal => println!("You guessed right"),
-      Ordering::Greater => println!("Too big")
+        println!("Your input is: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("You guessed right");
+                break;
+            }
+            Ordering::Greater => println!("Too big"),
+        }
     }
 }
