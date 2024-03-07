464f589524aa4c6b73b882082ad7fccd1d2f8b33
mindeleven <mindeleven@posteo.de>
2024-03-07 12:22:02 +0100

    New project to work along with conditionals and loops in chapter three

---
+++ ch03/ex0302_control_flow/src/main.rs
@@ -0,0 +1,100 @@
+fn main() {
+    // most common constructs for controling the flow of execution of Rust code 
+    // are if expressions and loops
+    println!("------------------------------------------------------------");
+    println!("if Expressions");
+    println!("------------------------------------------------------------");
+    // an if expression allows you to branch your code depending on conditions
+    // the condition must be a bool
+    // Rust will not try to convert non-boolean types to a boolean
+    // multiple conditions can be handeled by combining if and else in an else if expression
+    let number = 666;
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    println!("------------------------------------------------------------");
+    println!("Using if in a let Statement");
+    println!("------------------------------------------------------------");
+    // the potential result of each arm must be the same type
+    let condition = true;
+    let number_two = if condition {
+        27
+    } else {
+        42
+    };
+    println!("number two turned out to be {}", number_two);
+
+    println!("------------------------------------------------------------");
+    println!("Repetition with Loops");
+    println!("------------------------------------------------------------");
+    // loops will run through the code inside the loop body to the end 
+    // and then start immediately back at the beginning
+    let mut counter = 0;
+    loop {
+        if counter == 0 {
+            println!("LOOP 01 executing....")
+        }
+        println!("looping at conter {}", counter);
+        if counter == 6 {
+            println!("time to go....");
+            break;
+        }
+        counter += 1;
+    }
+
+    println!("------------------------------------------------------------");
+    println!("Returning Values from Loops");
+    println!("------------------------------------------------------------");
+    // passing the result out of an operation that got checked inside the loop
+    let mut counter = 0;
+    // declare a variable result to hold the value returned from the loop
+    let result = loop {
+        if counter == 0 {
+            println!("LOOP 02 executing....")
+        }
+        println!("looping at conter {}", counter);
+        if counter == 4 {
+            break counter * counter;
+        };
+        counter += 1;
+    };
+    println!("The number we've been looking for is {}", result);
+
+    println!("------------------------------------------------------------");
+    println!("Loop Labels to Disambiguate Between Multiple Loops");
+    println!("------------------------------------------------------------");
+    // usually break and continue apply to the innermost loop at that point
+    // alternatively you could specify a loop label on a loop 
+    // it can be used to apply break or continue the labeled loop
+    let mut counter = 0;
+    'counting_loop: loop {
+        println!("counter inside outer loop: {}", counter);
+        let mut inner_counter = 10;
+
+        loop {
+            println!("inner counter inside inner loop: {}", inner_counter);
+
+            if inner_counter == 0 {
+                break;
+            }
+
+            if counter == 2 {
+                break 'counting_loop;
+            }
+
+            inner_counter -= 1;
+        }
+
+
+        counter += 1;
+    }
+    
+
+}
