10fe315065a9c9460649ee4c3a46196063058206
mindeleven <mindeleven@posteo.de>
2024-03-08 10:01:12 +0100

    String type example

--- ch04/ex0401_ownership_rules/src/main.rs
+++ ch04/ex0401_ownership_rules/src/main.rs
@@ -9,8 +9,22 @@ fn main() {
     println!("------------------------------------------------------------");
     // s scope is the range within a program for which an item is valid
     {
-        let s = "hello"; // variable s refers to a string literal
+        let s = "hello"; // variable s refers to a string literal (immutable)
         println!("let's do something with s at least once: {}", s);
     } // this scope is now over, and s is no longer valid
+
+    println!("------------------------------------------------------------");
+    println!("The String Type");
+    println!("------------------------------------------------------------");
+    // to illustrate the rules of ownership it's better to look at data that's stored on the heap
+    // the String type is a complex data type that manages data allocated on the heap
+    // it is able to store an amount of text that is unknown to us at compile time
+    // creating a String from a string literal with the from function
+    let mut st = String::from("hello blue sky");
+    // push_str appends a literal to a String
+    st.push_str(" where the sun shines so bright");
+    println!("{}", st);
+
+    println!("------------------------------------------------------------");
 
 }
