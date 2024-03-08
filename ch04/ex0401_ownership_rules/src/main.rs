2cbb4479e2a16a878ca7d85aed1a5b6e0b19dcdc
mindeleven <mindeleven@posteo.de>
2024-03-08 14:41:16 +0100

    Memory and allocation on the heap

--- ch04/ex0401_ownership_rules/src/main.rs
+++ ch04/ex0401_ownership_rules/src/main.rs
@@ -26,5 +26,34 @@ fn main() {
     println!("{}", st);
 
     println!("------------------------------------------------------------");
+    println!("Memory and Allocation");
+    println!("------------------------------------------------------------");
+    // to support a mutable, growable piece of text we need to allocate an amount of memory on the heap
+    // the memory must be requested from the memory allocator at runtime
+    // Rust returns the memory automatically to the allocator
+    // once the variable that owns it goes out of scope
+    // Rust calls internally the drop function
+    {
+        let mut sx = String::from("hello blue sky");
+        sx.push_str(" where the sun shines so bright");
+    } // scope is over and sx is no longer valid
+    // moving data
+    {
+        let s1 = String::from("hello");
+        // when we assign s1 to s2 the String data in the stack is copied
+        // meaning we copy the pointer, the length & the capacity
+        // the data on the heap that the pointer refers to is not copied
+        // Rust invalidates the first variable -> s1 was moved into s2
+        // in Rust this is known as a move
+        let mut s2 = s1;
+        s2.push_str(" , world!");
+        // println!("{}, world!", s1); // compiler error: borrow of moved value: `s1`
+    }
+    // the method clone can be used if we want to copy the heap data
+    {
+        let s1 = String::from("hello from the heap");
+        let s2 = s1.clone();
+        println!("s1: {}, s2: {}", s1, s2)
+    }
 
 }
