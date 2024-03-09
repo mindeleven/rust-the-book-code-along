857a10b23903c70138173709038363e005019c0c
mindeleven <mindeleven@posteo.de>
2024-03-09 10:21:17 +0100

    A first example for passing a reference

--- ch04/ex0402_references_and_borrowing/src/main.rs
+++ ch04/ex0402_references_and_borrowing/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    println!("------------------------------------------------------------");
+    println!("References and Borrowing");
+    println!("------------------------------------------------------------");
+    // a reference is like a pointer in that you can follow to access the data stored at that address
+    // unlike with a pointer the  data is owned by some other variable
+    // a reference is guaranteed to point to a valid value of a particular type 
+    // for the life of that reference
+
+    // the opposite of referencing by using & is dereferencing
+    // with the dereference operator *
+
+    let s1 = String::from("The bluest sky");
+    // the argument passed to the function is a reference to s1
+    // the &s1 syntax creates a reference that refers to the value of s1 but does not own it
+    let s2 = calculate_length(&s1);
+
+    println!("The string \"{}\" has a length of {}.", s1, s2);
+}
+
+// function that has a reference to an object as a parameter 
+// and is not taking ownership of the value
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
