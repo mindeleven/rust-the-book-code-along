389f9244212de7c44b4fd47e835a8e1805239ec8
mindeleven <mindeleven@posteo.de>
2024-03-10 14:49:42 +0100

    Making things better with string slices

--- ch04/ex0403_slice_type/src/main.rs
+++ ch04/ex0403_slice_type/src/main.rs
@@ -13,6 +13,40 @@ fn main() {
     
     // if we rely on the index to extract the first word we can't do it anymore
     // s has still the value 5 but the contents of s have changed
+
+    println!("------------------------------------------------------------");
+    println!("Making things better with string slices");
+    println!("------------------------------------------------------------");
+    let s1 = String::from("Not with a bang but a whimper");
+    // a string slice is a reference to part of a String
+    let bang = &s1[11..15];
+    let whimper = &s1[22..];
+    // alternatively 
+    // let len = s1.len();
+    // let whimper = &s1[22..len];
+    let not = &s1[..3];
+    let all_of_it = &s1[..];
+    println!("{} {} but {}", not, bang, whimper);
+    println!("{}", all_of_it);
+    let s1 = String::from("This is the way the world ends");
+    println!("first word in the string is {:?}", find_first_slice(&s1));
+
+}
+
+// function for a second attempt using slices
+// return type that signifies string slice is written as &str
+fn find_first_slice(s: &String) -> &str {
+    // same procedure as with first attempt below
+    // to get the index for the end of the word
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            // this time we return a string slice 
+            // using the start of the string and the index of the space 
+           return &s[0..i];
+        }
+    }
+    &s[..]
 }
 
 // function for a first attempt without using slices
