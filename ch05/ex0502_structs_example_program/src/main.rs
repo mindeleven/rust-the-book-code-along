7760d8e79a24141fb4daa692aa644c2ed8f90aef
mindeleven <mindeleven@posteo.de>
2024-03-11 12:17:24 +0100

    Example program for demonstrating how to use structs to calculate the area of a rectangle

---
+++ ch05/ex0502_structs_example_program/src/main.rs
@@ -0,0 +1,8 @@
+/// example program that calculates the area of a rectangle
+/// let's start with using single variables and then refactor the program to use structs
+/// the rectangles program will take the width and height of a rectangle 
+/// specified in pixels and calculate the area of the rectangle
+
+fn main() {
+    println!("Hello, world!");
+}
