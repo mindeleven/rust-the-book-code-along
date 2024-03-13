c9036ce18240d763d6c94f097915ba3d1cfc771f
mindeleven <mindeleven@posteo.de>
2024-03-13 10:58:11 +0100

    Laying out the a module structure for a library crate that provides the functionality of a restaurant

--- ch07/ex0701_packages_crates_modules/src/lib.rs
+++ ch07/ex0701_packages_crates_modules/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+/// grouping related code in modules
+/// example: a library crate that provides the functionality of a restaurant
+/// "front of house" part of a restaurant is where customers are
+/// "back of house" part is where the chefs and cooks work in the kitchen
+/// to structure our crate in this way we organize its functions into nested modules
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// front of house module section
+/// the entire module tree is rooted under the implicit module named crate
+mod front_of_house {
+    // front of house is (1) where customers are hosted
+    // hosting nests inside front_of_house
+    // hosting is the child of module front_of_house 
+    // and that module front_of_house is the parent of module hosting
+    mod hosting {
+        fn _add_to_waitlist() {}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        fn _seat_at_table() {}
+    }
+    
+    // front of house is (2) where  servers take orders
+    // orders are served and payment are taken
+    // hosting and serving are siblings to each other
+    mod serving {
+        fn _take_order() {}
+
+        fn _serve_order() {}
+
+        fn _take_payment() {}
     }
 }
+
