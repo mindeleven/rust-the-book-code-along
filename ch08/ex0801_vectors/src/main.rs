cb5c9b093e6f3abb1ecf345f2d132b8b9c8ef261
mindeleven <mindeleven@posteo.de>
2024-03-14 12:40:36 +0100

    Accessing elements of a vector

--- ch08/ex0801_vectors/src/main.rs
+++ ch08/ex0801_vectors/src/main.rs
@@ -32,14 +32,33 @@ fn main() {
     // (1) via indexing
     // (2) using the guess method
     // accessing a value in a vector with indexing
+    // using & and [] gives us a reference to the element at the index value
     let third_element_of_v = &v[2];
     println!("the third element of v is {}", third_element_of_v);
     // accessing a value in a vector using the get method
-    // returns an Option
+    // returns an Option<T>
     let third_elem_by_get = v.get(2);
     match third_elem_by_get {
         Some(elem) => println!("The third element by get is {elem}"),
         None => println!("There is no third element."),
     }
+    // useful if you try to use an index value outside the range of existing elements
+    let tenth_elem_by_get = v.get(10);
+    match tenth_elem_by_get {
+        Some(elem) => println!("The 10th element by get is {elem}"),
+        None => println!("There is no 10th element."),
+    }
+
+    // when the program has a valid reference
+    // the borrow checker enforces the ownership and borrowing rules
+    let _fourth_element = &v[3]; // immutable borrow occurs here
+    // attempting to add an element to a vector while holding a reference to an item
+    v.push(109); // mutable borrow occurs here
+    // compiler error[E0502]: cannot borrow `v` as mutable because it is also borrowed as immutable
+    // println!("the 4th element is {}", _fourth_element); // immutable borrow later used here
+    // the rule states you can’t have mutable and immutable references in the same scope
+    // we hold an immutable reference to the fourth element in a vector 
+    // and try to add an element to the end
+    // this program won’t work if we also try to refer to that element later in the function
 
 }
