340b0f8783b6162bc8d4604b730ee3bbbbaa3c6b
mindeleven <mindeleven@posteo.de>
2024-03-15 14:55:04 +0100

    Three different ways of updating a hash map

--- ch08/ex0803_hash_maps/src/main.rs
+++ ch08/ex0803_hash_maps/src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("White"), 100);
 
-    println!("{:?}", scores);
+    println!("scores: {:?}", scores);
 
     // accessing values in a HashMap for a given key
     let team_member = String::from("Blue");
@@ -44,6 +44,39 @@ fn main() {
     map.insert(field_name, field_value); // field_name and field_value are invalid at this point
     println!("{:?}", map);
 
+    // Updating a Hash Map
+    // each unique key can only have one value associated with it at a time
+    // three different ways of handling the change of data in a hash map
+    // (1) Overwriting a Value
+    // using scores from above
+    let mut scores2 = HashMap::new();
+    scores2.insert(String::from("White"), 66);
+    println!("scores of team 2: {:?}", scores2); // {"White": 66}
+    scores2.insert(String::from("White"), 2);
+    println!("scores of team 2: {:?}", scores2); // {"White": 2}
+
+    // (2) Adding a Key and Value Only If a Key Isn’t Present
+    // Checking if a key exists with entry (takes the key you want to check as a parameter)
+    // or_insert() on Entry is defined to return a mutable reference 
+    // to the value for the corresponding Entry key if that key exists
+    // if not it inserts the parameter as the new value for this key
+    // and returns a mutable reference to the new value
+    scores2.entry(String::from("White")).or_insert(100); // doesn't get overwritten
+    scores2.entry(String::from("Blue")).or_insert(100);
+    println!("scores of team 2: {:?}", scores2); // {"White": 2, "Blue": 100}
+
+    // (3) Updating a Value Based on the Old Value
+    let text = "whisper our dried voices when we whisper our together";
+    let mut whisper_map = HashMap::new();
+    // split_whitespace() returns an iterator over sub-slices
+    for word in text.split_whitespace() {
+        // or_insert() returns a mutable reference (&mut V) to the value for the specified key
+        // we store that mutable reference in the count variable
+        let count = whisper_map.entry(word).or_insert(0);
+        // so in order to assign to that value we must first dereference count
+        *count += 1;
+    }
+    println!("{:?}", whisper_map);
 
 
 }
