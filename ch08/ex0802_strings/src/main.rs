bc895f1e8a07d165e245369f2623f6536b58b7a8
mindeleven <mindeleven@posteo.de>
2024-03-15 11:01:04 +0100

    Updating a string, combining strings

--- ch08/ex0802_strings/src/main.rs
+++ ch08/ex0802_strings/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     // Creating a New String
     // String is actually implemented as a wrapper around a vector of bytes
     // the new function to create an instance of string
-    let s = String::new();
+    let mut s = String::new();
 
     // string with a string literal initial content
     let s1 = "good afternoon".to_string();
@@ -31,5 +31,41 @@ fn main() {
     let hello10 = String::from("Здравствуйте");
     let hello11 = String::from("Hola");
     println!("{} {} {} {} {} {} {} {} {} {} {}", hello1, hello2, hello3, hello4, hello5, hello6, hello7, hello8, hello9, hello10, hello11);
+   
+    // Updating a String
+    // Appending to a String with push_str and push
+    // appending a string slice with push_str()
+    s.push_str("good morning");
+    println!("{}", s);
+   
+    let mut s5 = String::from("foo");
+    let s6 = "bar";
+    // push_str method takes a string slice 
+    // it doesn't take ownership of the parameter
+    s5.push_str(s6);
+    // s6 (the parameter) can be used afterwards
+    println!("{}, {}", s5, s6);
 
+    // push() takes a single character (char) as a parameter and adds it to String
+    let mut s7 = String::from("lo");
+    s7.push('l');
+    println!("{}", s7);
+
+    // Concatenation with the + Operator or the format! Macro
+    // (1) combining two existing strings with the + operator
+    let s8 = String::from("Hello, ");
+    let s9 = String::from("world!");
+    // s8 is moved and can no longer be used
+    let s10 = s8 + &s9; 
+    println!("{}", s10);
+    // the + operator uses the add method whose signature looks something like this
+    // fn add(self, s: &str) -> String 
+    
+    // using the format! macro for more complicated string combining
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{s1}-{s2}-{s3}");
+    println!("{}", s);
+   
 }
