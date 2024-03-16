a385db02108b984382fd78ef6e1e66eeb37832e1
mindeleven <mindeleven@posteo.de>
2024-03-16 11:54:33 +0100

    Using closures and the unwrap_or_else method as an alternatives match with Result

--- ch09/ex09_error_handling/src/main.rs
+++ ch09/ex09_error_handling/src/main.rs
@@ -4,6 +4,7 @@
 /// Rust has the type Result<T, E> for recoverable errors 
 /// and the panic! macro to stop execution in case of an unrecoverable error
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     // a simple call to panic
@@ -40,10 +41,47 @@ fn main() {
     let greeting_file_result = File::open("hello.txt");
     // handling the Result with the match expression 
     // that allows us take different actions depending on the value
-    let _greeting_file = match greeting_file_result {
+    /* 
+    let _greeting_file: File = match greeting_file_result {
         Ok(file_handle) => file_handle,
         Err(error) => panic!("Problem opening the file: {:?}", error),
     };
+    */
 
+    // Matching on Different Errors
+    // let greeting_file_result = File::open("hello.txt");
+    // different actions for different failure reasons
+    // if File::open failed because the file doesn’t exist -> create the file and return the handle
+    // if File::open failed for any other reason -> panic!
+    let _greeting_file = match greeting_file_result {
+        Ok(file_handle) => file_handle,
+        // File::open returns io::Error, a struct provided by the standard library
+        // struct has a method kind that we can call to get an io::ErrorKind
+        Err(error) => match error.kind() {
+            // enum io::ErrorKind is provided by the standard library 
+            // and has variants representing the different kinds of errors
+            // ErrorKind::NotFound indicates the file we’re trying to open doesn’t exist yet
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                // File::create might fail and returns an io::Result
+                Ok(file_handle) => file_handle,
+                Err(error) => panic!("creating opening the file: {:?}", error),
+            },
+            // program panics on any error besides the missing file error
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            },
+        }
+    };
+
+    // using closures and the unwrap_or_else method as an alternatives match with Result
+    let _greeting_file2 = File::open("hello2.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound { 
+            File::create("hello2.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
     
 }
