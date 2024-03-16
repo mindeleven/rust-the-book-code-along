3313676ea3d5b73c5d6e6b4b06dfa949bcb7bc9e
mindeleven <mindeleven@posteo.de>
2024-03-16 09:53:20 +0100

    Unrecoverable errors with panic

--- ch09/ex09_error_handling/src/main.rs
+++ ch09/ex09_error_handling/src/main.rs
@@ -1,3 +1,25 @@
+/// errors are grouped into two major categories: recoverable and unrecoverable errors
+/// (1) recoverable error -> they allow us to report the problem to the user and retry the operation
+/// (2) unrecoverable errors are always symptoms of bugs that make it necessary stop the program
+/// Rust has the type Result<T, E> for recoverable errors 
+/// and the panic! macro to stop execution in case of an unrecoverable error
+
 fn main() {
-    println!("Hello, world!");
+    // a simple call to panic
+    // panic!("crash and burn");
+
+    // Using a panic! Backtrace
+    let _v = vec![1, 2, 3]; 
+    // trying to access an index in a vector beyond the range of valid indexes
+    _v[66]; // thread 'main' panicked at ...
+    // compiler error:
+    /* 
+    thread 'main' panicked at src/main.rs:14:6:
+    index out of bounds: the len is 3 but the index is 66
+    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+    */
+    // -> we can set the RUST_BACKTRACE environment variable 
+    // to get a backtrace of exactly what happened to cause the error
+    // RUST_BACKTRACE=1 cargo run
+
 }
