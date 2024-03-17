17229cffacbcf0b524d9bad9508e510b5dac9a5e
mindeleven <mindeleven@posteo.de>
2024-03-17 14:41:23 +0100

    Revisiting the code of the guessing game from chapter 2

--- ch09/ex0902_to_panic_or_not/src/main.rs
+++ ch09/ex0902_to_panic_or_not/src/main.rs
@@ -3,6 +3,42 @@
 /// returning a Result value gives the calling code options
 /// the calling code could choose to attempt to recover in a way that’s appropriate
 
+use rand::Rng;
+use std::{
+    cmp::Ordering, 
+    io
+};
+
 fn main() {
-    println!("Hello, world!");
+    // Creating Custom Types for Validation
+    // let's go back to the code of the guessing game from chapter two
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+
+    println!("It's a guessing game!");
+    loop {
+        println!("Please enter a number:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Something went wrong");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("Your input is: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("You guessed right");
+                break;
+            }
+            Ordering::Greater => println!("Too big"),
+        }
+    }
 }
