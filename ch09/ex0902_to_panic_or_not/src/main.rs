c1b605cad65c07770f7936d36a27e7d7389f368d
mindeleven <mindeleven@posteo.de>
2024-03-17 15:06:26 +0100

    Creating a Guess type with a struct and putting the logic for the validation of the range in the struct's implementation

--- ch09/ex0902_to_panic_or_not/src/main.rs
+++ ch09/ex0902_to_panic_or_not/src/main.rs
@@ -9,6 +9,23 @@ use std::{
     io
 };
 
+// creating a new type and putting the validations in a function 
+// to create an instance of the type rather 
+pub struct Guess {
+    value: i32,
+}
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value)
+        }
+        Guess{ value }
+    }
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     // Creating Custom Types for Validation
     // let's go back to the code of the guessing game from chapter two
@@ -35,6 +52,11 @@ fn main() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        let guess = Guess::new(guess);
+
+        let guess = guess.value();
+
         // then add a check for the number being in range
         if guess < 1 || guess > 100 {
             println!("The secret number will be between 1 and 100.");
