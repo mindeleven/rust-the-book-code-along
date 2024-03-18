153075ff5b996d9c94e2cb2c7d9a9d25324e0079
mindeleven <mindeleven@posteo.de>
2024-03-18 09:07:17 +0100

    New chapter about generics, first an example for removing duplication of code without involving generic types

---
+++ ch10/ex10_removing_duplication/src/main.rs
@@ -0,0 +1,20 @@
+/// Removing Duplication by Extracting a Function
+/// example: removing duplication of code without involving generic types
+/// the goal here is to extract a function that replaces specific values with a placeholder
+/// that represents multiple values
+
+fn main() {
+    // short program the finds the largest number in a list
+    let number_list = vec![23, 76, 33, 44, 96, 15, 28];
+
+    let mut largest_number = &number_list[0];
+
+    for num in &number_list {
+        if num > largest_number {
+            largest_number = num;
+        }
+    }
+   
+    println!("The largest number in the list is {}.", largest_number);
+
+}
