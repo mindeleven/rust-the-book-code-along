0ddf13f91ba33bc1f9e171aadeedf6267f86513f
mindeleven <mindeleven@posteo.de>
2024-03-18 09:14:32 +0100

    Duplicating code to find largest number in a second list

--- ch10/ex10_removing_duplication/src/main.rs
+++ ch10/ex10_removing_duplication/src/main.rs
@@ -15,6 +15,17 @@ fn main() {
         }
     }
    
+    // duplicating code to find number in a second list
+    let number_list = vec![88, 129, 654, 729, 12, 59, 109];
+
+    let mut largest_number = &number_list[0];
+
+    for num in &number_list {
+        if num > largest_number {
+            largest_number = num;
+        }
+    }
+
     println!("The largest number in the list is {}.", largest_number);
 
 }
