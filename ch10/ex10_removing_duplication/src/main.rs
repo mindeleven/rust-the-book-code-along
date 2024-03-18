4f0359ac75bee3e69211962ca01531e7f2f38e02
mindeleven <mindeleven@posteo.de>
2024-03-18 09:21:17 +0100

    Extracting the code that finds the largest number into a function

--- ch10/ex10_removing_duplication/src/main.rs
+++ ch10/ex10_removing_duplication/src/main.rs
@@ -7,25 +7,25 @@ fn main() {
     // short program the finds the largest number in a list
     let number_list = vec![23, 76, 33, 44, 96, 15, 28];
 
-    let mut largest_number = &number_list[0];
+    let largest_number = find_largest(&number_list);
+    println!("The largest number in the list (1) is {}.", largest_number);
 
-    for num in &number_list {
-        if num > largest_number {
-            largest_number = num;
-        }
-    }
-   
     // duplicating code to find number in a second list
     let number_list = vec![88, 129, 654, 729, 12, 59, 109];
 
-    let mut largest_number = &number_list[0];
+    let largest_number = find_largest(&number_list);
+    println!("The largest number in the list (2) is {}.", largest_number);
 
-    for num in &number_list {
+}
+
+fn find_largest(list: &[i32]) -> &i32 {
+    let mut largest_number = &list[0];
+
+    for num in list {
         if num > largest_number {
             largest_number = num;
         }
     }
 
-    println!("The largest number in the list is {}.", largest_number);
-
+    largest_number
 }
