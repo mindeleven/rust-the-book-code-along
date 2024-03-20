5c50050a7e3ce12923879dd3748b35e7aca800c4
mindeleven <mindeleven@posteo.de>
2024-03-20 14:22:22 +0100

    Examples for calling Summarize on different struct instances

--- ch10/ex1002_traits/src/main.rs
+++ ch10/ex1002_traits/src/main.rs
@@ -41,14 +41,37 @@ impl Summary for Tweet {
 }
 
 fn main() {
+    // instantiating the Tweet struct and calling summarize on it
     let tweet = Tweet {
-        username: String::from("horse_ebooks"),
+        username: String::from("A. L. Crego"),
         content: String::from(
-            "of course, as you probably already know, people",
+            "The greatness of gif format is (paradoxically) in its limits.",
         ),
         reply: false,
         retweet: false,
     };
     println!("1 new tweet: {}", tweet.summarize());
+
+    // instantiating the NewsArticle struct and calling summarize on it
+    let article1 = NewsArticle {
+        headline: String::from("Goldman Sachs Sees More Institutions Diving Into Crypto — Says Bitcoin ETFs Prompt a 'Psychological Shift'"),
+        location: String::from("news.bitcoin.com, Finance"),
+        author: String::from("Kevin Helms"),
+        content: String::from(
+            "Goldman Sachs is seeing more institutions diving into crypto, the global investment bank's head of digital assets has revealed, noting that until now the bitcoin price action has been driven primarily by retail investors. \"But it's the institutions that we've started to see come in,\" he stressed, adding that the appetite has \"transformed.\"",
+        ),
+    };
+    println!("1 new news article: {}", article1.summarize());
+
+    let article1 = NewsArticle {
+        headline: String::from("Bitcoin Technical Analysis: Bearish Signals Amid Market Turbulence Put Traders on High Alert"),
+        location: String::from("news.bitcoin.com, Markets and Prices"),
+        author: String::from("Kevin Helms"),
+        content: String::from(
+            "Bitcoin, with its intraday low and high at $60,760 and $66,382 respectively, and currently trading above the $63,500 mark, showcases its durability and unpredictability. The price movements of bitcoin persist in the lead-up to the U.S. Federal Reserve's gathering on Wednesday afternoon.",
+        ),
+    };
+    println!("1 new news article: {}", article1.summarize());
+
 }
 
