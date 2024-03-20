72a96aeed8d5ef06ed450483f549049d09a2aa97
mindeleven <mindeleven@posteo.de>
2024-03-20 13:42:52 +0100

    Implementing the Summary trait on different struct examples

--- ch10/ex1002_traits/src/main.rs
+++ ch10/ex1002_traits/src/main.rs
@@ -13,7 +13,42 @@ pub trait Summary {
     fn summarize(&self) -> String;
 }
 
+/// Implementing a Trait on a Type
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+// implementation of the Summary trait on the NewsArticle struct
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+// implementation of the Summary trait on the Tweet struct
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
 
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
 }
+
