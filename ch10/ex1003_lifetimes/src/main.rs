de26464184aba7f91171ec2c723abc1b28f99f11
mindeleven <mindeleven@posteo.de>
2024-03-22 10:02:38 +0100

    Creating a function that takes two paremeters but does not compile because of 'expected named lifetime parameter' error

--- ch10/ex1003_lifetimes/src/main.rs
+++ ch10/ex1003_lifetimes/src/main.rs
@@ -10,6 +10,23 @@ fn main() {
     // fixing the code so it doesn't have a dangling reference
     let x = 5;
     let r = &x;
-    
+
     println!("r: {}", r);
+
+    // Generic Lifetimes in Functions
+    let string1 = String::from("abc");
+    let string2 = "wxyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+/// Generic Lifetimes in Functions
+/// function takes string slices (which are references) rather than strings
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
