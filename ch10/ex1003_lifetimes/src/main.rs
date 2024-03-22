c6167380e7a1a39c33a85d1c2ae21dc25fc40c46
mindeleven <mindeleven@posteo.de>
2024-03-22 09:54:21 +0100

    First creating an example program with a dangling reference that doesn't compile

--- ch10/ex1003_lifetimes/src/main.rs
+++ ch10/ex1003_lifetimes/src/main.rs
@@ -1,5 +1,13 @@
 fn main() {
     // Main aim if lifetimes: preventing dangling references
+    // example program with a dangling reference
+    // the borrow checker compares scopes to determine whether all borrows are valid
+    let r;
 
-    println!("Hello, world!");
+    {
+        let x = 5;
+        let r = &x;
+    }
+
+    println!("r: {}", r);
 }
