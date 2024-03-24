a2516fd53ff76ec4412ef95cb01a5acdffac0171
mindeleven <mindeleven@posteo.de>
2024-03-24 10:19:32 +0100

    New project for coding along with part 1 of chapter 13: closures

---
+++ ch13/ex1301_closures/src/main.rs
@@ -0,0 +1,11 @@
+/// Closures are 
+/// -> a function-like construct you can store in a variable
+/// -> anonymous functions that capture their environment
+/// -> can be passed as arguments to other functions
+/// -> closures can capture values from the scope in which they’re defined
+
+/// Capturing the Environment with Closures
+
+fn main() {
+    println!("Hello, world!");
+}
