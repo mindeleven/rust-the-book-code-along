3243c94e59a10833e8e8c924e7c5f30161e08b85
mindeleven <mindeleven@posteo.de>
2024-03-26 11:19:08 +0100

    Some examples with cargo test

--- ch11/ex1101_writing_tests/src/lib.rs
+++ ch11/ex1101_writing_tests/src/lib.rs
@@ -5,17 +5,17 @@
 /// -> runs the code you want to test
 /// -> asserts the results are what you expect
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn exploration() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn another() {
+        panic!("Make this test fail!");
     }
 }
