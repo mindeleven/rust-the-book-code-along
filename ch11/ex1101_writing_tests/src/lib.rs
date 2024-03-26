ac6991ce2487a4d2bbfc39391f9255e564b353d6
mindeleven <mindeleven@posteo.de>
2024-03-26 11:39:10 +0100

    Checking results with the assert! macro

--- ch11/ex1101_writing_tests/src/lib.rs
+++ ch11/ex1101_writing_tests/src/lib.rs
@@ -5,9 +5,22 @@
 /// -> runs the code you want to test
 /// -> asserts the results are what you expect
 
+/// as an example once again the rectangle struct from chapter 5
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::*;    
 
     #[test]
     fn exploration() {
@@ -18,4 +31,36 @@ mod tests {
     fn another() {
         panic!("Make this test fail!");
     }
+
+    // Checking results with the assert! macro
+    // the assert! macro can ensure that some condition in a test evaluates to true
+    // writing a test that exercises the can_hold method by creating a Rectangle instance
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_can_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+        // the correct result of the can_hold function in this case is false
+        // we need to negate that result before we pass it to the assert! macro
+        assert!(!smaller.can_hold(&larger));
+    }
+
 }
