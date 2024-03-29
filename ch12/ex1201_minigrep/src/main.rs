ab81bf86fc2a3dfbaf1e5c31415ee5c90ef4b21b
mindeleven <mindeleven@posteo.de>
2024-03-29 09:37:03 +0100

    Adding sone comments to give an overview of what we want to achieve in this chapter

--- ch12/ex1201_minigrep/src/main.rs
+++ ch12/ex1201_minigrep/src/main.rs
@@ -1,3 +1,15 @@
+/// An I/O Project: Building a Command Line Program
+/// Coding along with chapter 12 of the "The Rust Programming Language" book
+/// 
+/// goal: building a command line tool that interacts with file and command line input/output
+/// it will be a simplified version of the classic command line search tool grep 
+/// grep == globally search a regular expression and print
+/// simplest use case: grep searches a specified file for a specified string
+/// -> (1) grep takes as its arguments a file path and a string
+/// -> (2) it reads the file
+/// -> (3) it finds lines in that file that contain the string argument
+/// -> (4) it prints those lines
+
 fn main() {
     println!("Hello, world!");
 }
