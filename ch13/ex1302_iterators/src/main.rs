cc50a22860c3e4bf71705d4224453a7fbcbc6a97
mindeleven <mindeleven@posteo.de>
2024-04-03 13:35:51 +0200

    Calling the next method on an iterator created from a vector and consuming adaptors (methods that consume the iterator by calling next)

--- ch13/ex1302_iterators/src/main.rs
+++ ch13/ex1302_iterators/src/main.rs
@@ -1,3 +1,51 @@
+/// all iterators implement a trait named Iterator 
+/// this trait is defined in the standard library and looks like this:
+/*
+pub trait Iterator {
+    type Item; // the Item type will be the type returned from the iterator
+
+    fn next(&mut self) -> Option<Self::Item>;
+
+    // methods with default implementations elided
+}
+ */
+
 fn main() {
-    println!("Hello, world!");
+    // iterators are lazy
+    // they have no effect until you call methods that consume them
+    let v1 = vec![1, 2, 3];
+    // iterator is just stored in the v1_iter variable
+    let v1_iter = v1.iter();
+    // using each element of the iterator in a for loop
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+}
+
+#[test]
+// calling the next method on an iterator created from a vector
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
+
+#[test]
+// Methods that consume the iterator
+// methods that call next (like the sum method) are called consuming adaptors
+// calling them uses up the iterator
+// sum takes ownership of the iterator and iterates through the items by repeatedly calling next
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum(); // sum takes ownership of the iterator
+
+    assert_eq!(total, 6);
 }
