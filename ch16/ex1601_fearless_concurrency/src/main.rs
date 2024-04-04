25612b3ce04579515429d4f3bf328cfd55745c0e
mindeleven <mindeleven@posteo.de>
2024-04-04 09:55:48 +0200

    Added explanatory comments

--- ch16/ex1601_fearless_concurrency/src/main.rs
+++ ch16/ex1601_fearless_concurrency/src/main.rs
@@ -1,3 +1,18 @@
+/// concurrent programming, 
+/// -> where different parts of a program execute independently
+/// parallel programming
+/// -> where different parts of a program execute at the same time
+/// 
+/// topics to be covered:
+/// -> how to create threads to run multiple pieces of code at the same time
+/// -> message-passing concurrency, where channels send messages between threads
+/// -> shared-state concurrency, where multiple threads have access to some piece of data
+/// -> the Sync and Send traits, 
+///    which extend Rust’s concurrency guarantees to user-defined types 
+///    as well as types provided by the standard library
+
+mod m1_threads;
+
 fn main() {
     println!("Coding along with The Rust Programming Language Book, Chapter 16");
     println!("Fearless Concurrency")
