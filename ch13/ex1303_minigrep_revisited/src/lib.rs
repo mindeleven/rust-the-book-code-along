adca638094421ab9b6da082ec9d17f193d2de886
mindeleven <mindeleven@posteo.de>
2024-04-05 10:21:34 +0200

    Using iterator adaptor methods in the implementation of the case insensitive search function

--- ch13/ex1303_minigrep_revisited/src/lib.rs
+++ ch13/ex1303_minigrep_revisited/src/lib.rs
@@ -97,21 +97,15 @@ pub fn search_case_insensitive<'a>(
     query: &str, 
     contents: &'a str
 ) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
     // calling to_lowercase creates new data rather than referencing existing data
     // query is becomes a String rather than a string slice
     let query = query.to_lowercase();
 
-    for line in contents.lines() {
-        // signature of contains is defined to take a string slice
-        // so query needs to be passed as a reference
-        if line.to_lowercase().contains(&query) {    
-            results.push(line);
-        }
-    }
+    contents
+        .lines()
+        .filter(|line| { line.to_lowercase().contains(&query) })
+        .collect()
 
-    results
 }
 
 /// test-driven development (TDD):
