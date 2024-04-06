016fbd404dc83ade29219e715c4d056c711a1313
mindeleven <mindeleven@posteo.de>
2024-04-06 09:36:45 +0200

    New project for coding along with chapter 16.3., shared-state concurrency

---
+++ ch16/ex1606_shared_state_concurrency/src/main.rs
@@ -0,0 +1,13 @@
+/// Using mutexes to allow access to data from one thread at a time
+use std::sync::Mutex;
+
+fn main() {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+}
