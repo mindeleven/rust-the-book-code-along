a8d797b42aed4e247beffdb68fd207a07364f17f
mindeleven <mindeleven@posteo.de>
2024-04-07 14:57:36 +0200

    Adding an explanatory comment

--- ch14/add/adder/src/main.rs
+++ ch14/add/adder/src/main.rs
@@ -1,3 +1,7 @@
+/// to run the binary crate from the add directory
+/// we can specify which package in the workspace we want to run 
+/// by using the -p argument and the package name with cargo run:
+/// `$ cargo run -p adder`
 use add_one;
 
 fn main() {
