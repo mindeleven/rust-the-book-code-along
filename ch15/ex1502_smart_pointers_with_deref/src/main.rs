03b4b6ec08b2ee243ccc8b1be3466a8619f0073d
mindeleven <mindeleven@posteo.de>
2024-04-09 14:10:49 +0200

    Using a Box<T> instead of a reference

--- ch15/ex1502_smart_pointers_with_deref/src/main.rs
+++ ch15/ex1502_smart_pointers_with_deref/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
     let x = 5;
-    let y = &x;
+    let y = Box::new(x);
 
     assert_eq!(5, x);
+    // to make an assertion about the value in y we have to dereference it
+    // a number and a reference to a number are of different types
+    // we can't compare them
     assert_eq!(5, *y);
 }
