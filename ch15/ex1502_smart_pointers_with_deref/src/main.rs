d7d5d23f63470fb768399e2027c59885fed8ad25
mindeleven <mindeleven@posteo.de>
2024-04-09 14:29:07 +0200

    Treating a type like a reference by implementing the deref trait

--- ch15/ex1502_smart_pointers_with_deref/src/main.rs
+++ ch15/ex1502_smart_pointers_with_deref/src/main.rs
@@ -2,6 +2,8 @@
 /// building a smart pointer similar to the Box<T>
 /// Box<T> type is defined as a tuple struct with one element
 /// we're doing the same with MyBox<T>
+use std::ops::Deref;
+
 struct MyBox<T>(T);
 
 impl<T> MyBox<T> {
@@ -10,6 +12,21 @@ impl<T> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
         MyBox(x)
     } 
+}
+
+/// to dereference the MyBox<T> type we have to implement the Deref ability on our type
+/// implementating Deref as an addition to the definition of MyBox
+/// the Deref trait requires us to implement one method named deref 
+/// deref() borrows self and returns a reference to the inner data
+impl<T> Deref for MyBox<T> {
+    // defining an associated type for the Deref trait to use
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        // we return &self.0 so that deref returns a reference to the value 
+        // we want to access with the * operator
+        &self.0
+    }
 }
 
 fn main() {
