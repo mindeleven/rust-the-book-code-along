f95d4cac2f956ca4f56aa61c91b79a73f2835ead
mindeleven <mindeleven@posteo.de>
2024-04-09 14:19:10 +0200

    building a custom smart pointer similar to Box<T>

--- ch15/ex1502_smart_pointers_with_deref/src/main.rs
+++ ch15/ex1502_smart_pointers_with_deref/src/main.rs
@@ -1,6 +1,20 @@
+#![allow(dead_code, unused_variables)]
+/// building a smart pointer similar to the Box<T>
+/// Box<T> type is defined as a tuple struct with one element
+/// we're doing the same with MyBox<T>
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    // MyBox::new function takes one parameter of type T 
+    // and returns a MyBox instance that holds the value passed in
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    } 
+}
+
 fn main() {
     let x = 5;
-    let y = Box::new(x);
+    let y = MyBox::new(x);
 
     assert_eq!(5, x);
     // to make an assertion about the value in y we have to dereference it
