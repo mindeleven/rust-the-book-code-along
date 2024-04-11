766d4417e0865cc7ddf91fadd16892aba5318532
mindeleven <mindeleven@posteo.de>
2024-04-11 13:04:50 +0200

    Increasing the reference count by cloning an Rc<T>

--- ch15/ex1504_reference_counted_smart_pointer/src/main.rs
+++ ch15/ex1504_reference_counted_smart_pointer/src/main.rs
@@ -29,5 +29,20 @@ fn main() {
     // Rc::clone doesn’t make a deep copy of all the data
     let b = Cons(3, Rc::clone(&a));
     let c = Cons(3, Rc::clone(&a));
+
+    // Cloning an Rc<T> increases the reference count
+    // slightly modified working example
+    // printing the reference count by calling the Rc::strong_count function
+    let a1 = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a1 = {}", Rc::strong_count(&a1)); // 1
+    let b1 = Cons(3, Rc::clone(&a1));
+    // each time we call clone, the count goes up by 1
+    println!("count after creating b1 = {}", Rc::strong_count(&a1)); // 2
+    {
+        // inner scope around list c1
+        let c1 = Cons(4, Rc::clone(&a1));
+        println!("count after creating c1 = {}", Rc::strong_count(&a1)); // 3
+    }
+    println!("count after c1 goes out of scope = {}", Rc::strong_count(&a1)); // 2
 }
 
