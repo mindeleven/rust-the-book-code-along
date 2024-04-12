b54d0157c7fde5ce358f4160df8e5a1e3cdfa81e
mindeleven <mindeleven@posteo.de>
2024-04-12 10:31:40 +0200

    New project for some in-depth learning about Box, Rc, Arc &  Mutex

---
+++ ch15/in_depth_learning/smart_pointers_simplified/src/main.rs
@@ -0,0 +1,12 @@
+/// coding along with the youtube video 
+/// "Box / Rc / Arc / Mutex - Smart Pointers Simplified - Rust" by Bocksdin Coding
+/// @ https://www.youtube.com/watch?v=mNHdD69iLzA
+
+fn main() {
+    // let's begin with a box 
+    // a box takes a piece of data and places it on the heap
+    // especially helpful when you don't know the amount of memory you need
+    // at the time you're writing and compiling your code
+    // example: link list that can have a potentially infinite size
+
+}
