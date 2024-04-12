5f044418802996409570eb25c558bb38601f4171
mindeleven <mindeleven@posteo.de>
2024-04-12 12:18:01 +0200

    Example of a Rc<> and creating a new reference to an existing peace of data

--- ch15/in_depth_learning/smart_pointers_simplified/src/main.rs
+++ ch15/in_depth_learning/smart_pointers_simplified/src/main.rs
@@ -26,8 +26,36 @@ struct LinkListNode {
     next: Option<Box<LinkListNode>>
 }
 
+/// let's define Rc<>
+/// Rc stands for reference counter but it's more like a "reference copier"
+/// an Rc creates a new reference to an existing peace of data
+/// you can have two refrences that are owned separately and point both at the same structure
+use std::rc::Rc;
 
+/// example: one owner can have many tools
+/// a tool can have only one owner
+struct Owner {
+    name: String,
+    tools: Vec<Tool>
+}
+
+struct Tool {
+    // to create a reference around brad we define owner as type Rc<>
+    owner: Rc<Owner>
+}
 
 fn main() {
+    // let brad = Owner { name: "Brad".to_string(), tools: vec![] };
+    // to define the brad is a Rc we need to wrap it
+    let brad = Rc::from(Owner { name: "Brad".to_string(), tools: vec![] });
+    // let pliers = Tool { owner: brad }; // brad has been moved to pliers
+    // let wrench = Tool { owner: brad}; // error: use of moved value: `brad`
+    // the way around is is to create a reference around brad
+    // creating a new reference with Rc::clone()
+    let wrench = Tool { owner: Rc::clone(&brad) };
+    // another way to do it:
+    let pliers = Tool { owner: brad.clone() };
+
+
     println!("cats like boxes");
 }
