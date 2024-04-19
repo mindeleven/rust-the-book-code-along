41598f22f7cd40a3bf7b51aded49c83e528c8428
mindeleven <mindeleven@posteo.de>
2024-04-19 11:15:31 +0200

    New project for working along with section 3 of chapter 17, Implementing an Object-Oriented Design Pattern

---
+++ ch17/ex1703_state_design_pattern/src/main.rs
@@ -0,0 +1,13 @@
+/// Implementing an Object-Oriented Design Pattern
+/// 
+/// the state pattern is an object-oriented design pattern
+/// -> in this pattern we define a set of states a value can have internally
+/// -> the states are represented by a set of state objects
+/// -> the value’s (?) behavior changes based on its state
+/// 
+/// example: a blog post struct that can have the states "draft", "review" or "published"
+
+
+fn main() {
+    println!("Hello, world!");
+}
