e523f5b755ea4f77c90554172eeb49da272c1272
mindeleven <mindeleven@posteo.de>
2024-04-23 11:00:37 +0200

    Added some explanatory comments

--- ch20/ex2001_single_threaded_web_server/src/main.rs
+++ ch20/ex2001_single_threaded_web_server/src/main.rs
@@ -1,12 +1,19 @@
+#![allow(unused_variables)]
+
 /// Building a Single-Threaded Web Server
 
 use std::net::TcpListener;
 
 fn main() {
     // listening to a TCP connection
+    // at the local address 127.0.0.1:7878 for incoming TCP streams
+    // bind will return a new TcpListener instance and returns a Result<T, E>
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     
+    // the incoming method on TcpListener returns an iterator 
+    // with a sequence of streams of type TcpStream
     for stream in listener.incoming() {
+        // single stream represents an open connection between the client and the server
         let stream = stream.unwrap();
 
         println!("Connection established!");
