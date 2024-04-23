048f46e525a47921b95f8829dc37a3aa3723f923
mindeleven <mindeleven@posteo.de>
2024-04-23 09:44:20 +0200

    Conditional if let expressions

--- ch18/ex1801_patterns_matching/src/main.rs
+++ ch18/ex1801_patterns_matching/src/main.rs
@@ -1,3 +1,34 @@
+use std::error::Error;
+
 fn main() {
-    println!("Hello, world!");
+
+    // Conditional if let Expressions
+    // if let expressionscan be used to write the equivalent of a match 
+    // that only matches one case
+    // optionally, if let can have a corresponding else 
+
+    // example: determining what color to make your background 
+    // based on a series of checks for several conditions
+    let favourite_color: Option<&str> = None;
+    // let favourite_color: Option<&str> = Some("black");
+    let is_tuesday = false;
+    // let is_tuesday = true;
+    let age: Result<u8, _> = "34".parse();
+    let age: Result<u8, u8> = Err(1);
+    
+    if let Some(color) = favourite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        } 
+    } else {
+        println!("Something is wrong here!");
+        println!("Using blue as the background color");
+    }
+    
 }
