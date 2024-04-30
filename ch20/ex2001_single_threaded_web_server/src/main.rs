3c568d5053ace639f6ad5aa5f5f124e8f1b8a96b
mindeleven <mindeleven@posteo.de>
2024-04-30 11:13:07 +0200

    Implementing functionality to read the request from the browser

--- ch20/ex2001_single_threaded_web_server/src/main.rs
+++ ch20/ex2001_single_threaded_web_server/src/main.rs
@@ -2,7 +2,16 @@
 
 /// Building a Single-Threaded Web Server
 
-use std::net::TcpListener;
+use std::{
+    net::{
+        TcpListener, 
+        TcpStream
+    },
+    io::{
+        prelude::*,
+        BufReader
+    }, 
+};
 
 fn main() {
     // listening to a TCP connection
@@ -12,6 +21,7 @@ fn main() {
     
     // the incoming method on TcpListener returns an iterator 
     // with a sequence of streams of type TcpStream
+    // echo "TEST" | netcat 127.0.0.1 7878
     for stream in listener.incoming() {
         // single stream represents an open connection between the client and the server
         let stream = stream.unwrap();
@@ -20,4 +30,15 @@ fn main() {
     }
 
     println!("The sky above the port was the color of television, tuned to a dead channel.");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader 
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
