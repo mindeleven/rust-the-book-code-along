7e7740f8dacd380768a7006c0aa0cc75f0b2e91b
mindeleven <mindeleven@posteo.de>
2024-05-18 10:35:03 +0200

    Implementing the Add trait to overload the + operator for Point instances

--- ch19/ex1902_advanced_traits/src/main.rs
+++ ch19/ex1902_advanced_traits/src/main.rs
@@ -13,6 +13,35 @@ pub trait Iterator {
     fn next(&mut self) -> Option<Self::Item>;
 }
 
+/// Default Generic Type Parameters and Operator Overloading
+/// specifying a default concrete type for the generic type
+/// declaring a generic type with the <PlaceholderType=ConcreteType> syntax
+use std::ops::Add;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+/// overloading the operations and corresponding traits listed in std::ops 
+/// by implementing the traits associated with the operator
+/// example: overloading the + operator to add two Point instances together
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, other: Point) -> Point {
+        Point {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(
+        Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
+        Point { x: 3, y: 3 }
+    );
+
+    println!("Nothing to see here, please move on");
 }
