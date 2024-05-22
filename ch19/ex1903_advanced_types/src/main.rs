4aca793b6507b14da1ece63cf701684df967f34f
mindeleven <mindeleven@posteo.de>
2024-05-22 10:01:46 +0200

    Minor modifications to comments

--- ch19/ex1903_advanced_types/src/main.rs
+++ ch19/ex1903_advanced_types/src/main.rs
@@ -1,3 +1,19 @@
+/// Using the newtype pattern to implement external traits on external types
+use std::fmt;
+
+/// the newtype pattern is a lightweight way to achieve encapsulation to hide implementation details
+/// kind of a workaround for implementing Display on Vec<T>:
+/// (1) defining a Wrapper tuple struct that holds an instance of Vec<T>
+struct Wrapper(Vec<String>);
+/// (2) then implementing Display on Wrapper and using the Vec<T>
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // the implementation of Display uses self.0 to access the inner Vec<T>
+        write!(f, "[{}]", self.0.join(", "))
+    }   
+}
+
 fn main() {
-    println!("Hello, world!");
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
