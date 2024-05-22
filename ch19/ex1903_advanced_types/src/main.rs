0b353e390f487d6d90cab201448552c0d87e1df7
mindeleven <mindeleven@posteo.de>
2024-05-22 10:10:16 +0200

    Using type aliases

--- ch19/ex1903_advanced_types/src/main.rs
+++ ch19/ex1903_advanced_types/src/main.rs
@@ -16,4 +16,25 @@ impl fmt::Display for Wrapper {
 fn main() {
     let w = Wrapper(vec![String::from("hello"), String::from("world")]);
     println!("w = {}", w);
+
+    // Creating type synonyms with type aliases
+    // creating the alias Kilometers to i32
+    type Kilometers = i32;
+
+    let x: i32 = 5;
+    let y: Kilometers = 10;
+    // Kilometers and i32 are the same type
+    println!("x + y = {}", x + y);
+
+    // main use case for type synonyms: reducing repetition
+    // (= reducing typing when it comes to lengthy types)
+    type Thunk = Box<dyn Fn() + Send + 'static>;
+    let _f: Thunk = Box::new(|| println!("hi"));
+    // as function parameter or return type:
+    fn _takes_long_type(f: Thunk) {
+       unimplemented!()
+    }
+    fn _returns_long_type() -> Thunk {
+        unimplemented!()
+    }
 }
