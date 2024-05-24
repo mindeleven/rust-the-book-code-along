4b045e98eb8062b03a0ca211c7172bc91d104389
mindeleven <mindeleven@posteo.de>
2024-05-24 09:51:55 +0200

    Example where you could use either a closure defined inline or a named function

--- ch19/ex1904_advanced_functions/src/main.rs
+++ ch19/ex1904_advanced_functions/src/main.rs
@@ -14,6 +14,19 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
 
 fn main() {
     let res = do_twice(add_one, 332);
-
     println!("If we do it twice we get {}", res);
+
+    // example where you could use either a closure defined inline or a named function
+    // using the map function to turn a vector of numbers into a vector of strings
+    // closure approach
+    let list_of_numbers = vec![1, 2, 3];
+    let list_of_strings: Vec<String> =
+        list_of_numbers.iter().map(|i| i.to_string()).collect();
+    println!("The list of strings we got with a closure {:?}", list_of_strings);
+    // function pointer approach
+    let list_of_numbers_2 = vec![1, 2, 3];
+    let list_of_strings_2: Vec<String> =
+        list_of_numbers_2.iter().map(ToString::to_string).collect();
+        println!("The list of strings we got with a function pointer {:?}", list_of_strings_2);
+
 }
