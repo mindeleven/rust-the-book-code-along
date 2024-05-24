8a0940dc08b5511e16301a4219f1d1a03e89a05d
mindeleven <mindeleven@posteo.de>
2024-05-24 09:46:41 +0200

    Using the fn type to accept a function pointer as an argument

--- ch19/ex1904_advanced_functions/src/main.rs
+++ ch19/ex1904_advanced_functions/src/main.rs
@@ -1,5 +1,19 @@
 /// Advanced Functions and Closures
+/// 
+/// Function Pointers
+/// syntax for specifying that a parameter is a function pointer
+fn add_one(x: i32) -> i32 {
+    x+ 1
+}
+/// following function takes two parameters: 
+/// (1) a function pointer to any function that takes an i32 parameter and returns an i32
+/// (2) one i32 value
+fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
+    f(arg) + f(arg)
+}
 
 fn main() {
-    println!("Hello, world!");
+    let res = do_twice(add_one, 332);
+
+    println!("If we do it twice we get {}", res);
 }
