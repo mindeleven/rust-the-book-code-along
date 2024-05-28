376209175f554e5e9fa8bdc99715c01777bc98b4
mindeleven <mindeleven@posteo.de>
2024-05-28 09:44:10 +0200

    Additional project with cargo to add the two macro crates to a user project

---
+++ ch19/ex1905a_declarative_macros/hello_macro_user/src/main.rs
@@ -0,0 +1,24 @@
+use hello_macro::HelloMacro;
+
+/// a first approach on how to implement the trait to achieve the desired functionality
+struct Pancakes;
+
+impl HelloMacro for Pancakes {
+    fn hello_macro() {
+        println!("Hello, Macro! My name is Pancakes!");
+    }
+}
+
+// 2nd approach
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct Pancakes2;
+
+fn main() {
+    // first approach
+    Pancakes::hello_macro();
+    
+    // 2nd approach
+    Pancakes2::hello_macro();
+}
